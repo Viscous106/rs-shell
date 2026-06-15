883c7a29d7291ed1526946f1aa963c57de2e9d39
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-06-15 16:38:55 +0530

    fix: Correct namespace resolution path for std::process::Command

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
                     let target = args[0];
                     if target == "echo" || target == "exit" || target == "type"{
                         println!("{} is a shell builtin",target);
-                    }else if let Some(path) = get_executable_path(target)
+                    }else if let Some(path) = get_executable_path(target){
                         println!("{} is {}",target,path.display());//this is the path of the file
                     }else{
                         println!("{}: not found",target);
@@ -60,7 +60,7 @@ fn main() {
             }
         _ => {
             if get_executable_path(cmd).is_some(){
-                if let Err(e) = std::process::Command:new(cmd).args(args).status(){
+                if let Err(e) = std::process::Command::new(cmd).args(args).status(){
                     eprintln!("Failed to execute process: {}",e);
                 }
             }else{
