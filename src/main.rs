0f252ea929a4986959c60702a212f30053300b17
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-06-15 16:38:54 +0530

    feat: Implement binary resolution and path searching for type command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::path::PathBuf;
+use std::os::unix::fs::PermissionsExt;
+
+fn get_executable_path(cmd: &str) -> Option<PathBuf> {
+    if let Ok(path_env) = std::env::var("PATH") {
+        for dir in std::env::split_paths(&path_env) {
+            let full_path = dir.join(cmd);
+            if let Ok(metadata) = std::fs::metadata(&full_path) {
+                if metadata.is_file() && (metadata.permissions().mode() & 0o111 != 0) {
+                    return Some(full_path);
+                }
+            }
+        }
+    }
+    None
+}
 
 fn main() {
     loop{
@@ -7,15 +23,44 @@ fn main() {
         io::stdout().flush().unwrap();
         
         let mut command = String::new();
-        io::stdin().read_line(&mut command).uelse if command.starts_with("echo"){
-            println!("{}",&command[5..]);
-        }nwrap();
-        command = command.trim().to_string();
+        io::stdin().read_line(&mut command).unwrap();
+        let command = command.trim();
+
+        if command.is_empty(){
+            continue;
+        }
+        let parts: Vec<&str> = command.split_whitespace().collect();
+        let cmd = parts[0];
+        let args = &parts[1..];
         
-        if command.starts_with("type") && &command[5..] == "echo" || &command[5..] == "exit"{
-                println!("{} is a shell builtin",&command[5..]);
-        }else{
-            println!("{}: command not found",command.trim());
+        match cmd{
+            "exit" => {
+                let code = if args.is_empty(){
+                    0
+                }else {
+    	            args[0].parse::<i32>().unwrap_or(0)
+                };
+                std::process::exit(code);
+            }
+            "echo" => {
+                println!("{}",args.join(" "));
+            }
+            "type" => {
+                if args.is_empty(){
+                }else{
+                    let target = args[0];
+                    if target == "echo" || target == "exit" || target == "type"{
+                        println!("{} is a shell builtin",target);
+                    }else if let Some(path) = get_executable_path(target)(
+                        println!("{} is {}",target,path.display());//this is the path of the file
+                    )else{
+                        println!("{}: not found",target);
+                    }
+                }
+            }
+        }
+        _ => {
+            println!("{}:command not found",cmd);
         }
     }
 }
