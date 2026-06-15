bab1e196ab8879dbb8e6916be610f61aec711bc6
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-06-15 16:38:52 +0530

    feat: Handle exit command to break from REPL loop

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ fn main() {
         
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
+        if command == "exit"{
+            break;
+        }
         println!("{}: command not found",command.trim());
     }
 }
